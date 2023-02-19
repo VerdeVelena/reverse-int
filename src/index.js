3c092fdf13c96e058b4bca242a5917f0b9224b22
Roman Casian <verde.velena@gmail.com>
2023-02-19 21:29:10 +0100

    feat:function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const num = Math.abs(n);
+    const numReversed = num.toString().split("").reverse().join("");
+    return numReversed;
+};
